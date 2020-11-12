a196aa2f44f3e535c7838572f438c578fb3a3b60
N1Anoobis <mrbond84@hotmail.com>
2020-11-12 23:38:08 +0100

    Add try to deploy

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 /* MIDDLEWARE */
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 
 /* API ENDPOINTS */
