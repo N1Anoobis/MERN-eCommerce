80b61885e2ec6c6d629e4a0b38989de3bddae614
N1Anoobis <mrbond84@hotmail.com>
2020-11-16 15:49:16 +0100

    Add env DB

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api', (req, res) => {
   res.status(404).send({ data: 'Not found...' });
 });
 
-const db = (`mongodb+srv://slawomir:energy2000@cluster0.rqbyt.mongodb.net/ShopItemsDB?retryWrites=true&w=majority`);
+const db = process.env.DB;
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
